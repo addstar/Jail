4cae86fb4e3b1c5b1ce288b1598ebe098e8eaab7
graywolf336 <graywolf336@craftyn.com>
2013-09-14 23:05:58 -0500

    Work some on the Jail/Cell creation, not done and not happy with it.
    Not too happy with how this all turned out, if you have any suggestions
    on how to make it better feel free to do so or if you have any other way
    of doing it then let's talk.

--- src/com/graywolf336/jail/JailManager.java
+++ src/com/graywolf336/jail/JailManager.java
@@ -3,13 +3,18 @@ package com.graywolf336.jail;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import com.graywolf336.jail.beans.CreationPlayer;
 import com.graywolf336.jail.beans.Jail;
 
 public class JailManager {
 	private HashMap<String, Jail> jails;
+	private HashMap<String, CreationPlayer> jailCreators;
+	private HashMap<String, CreationPlayer> cellCreators;
 	
 	public JailManager() {
 		this.jails = new HashMap<String, Jail>();
+		this.jailCreators = new HashMap<String, CreationPlayer>();
+		this.cellCreators = new HashMap<String, CreationPlayer>();
 	}
 	
 	/** Returns a HashSet of all the jails. */
@@ -40,5 +45,69 @@ public class JailManager {
 	 */
 	public boolean isValidJail(String name) {
 		return this.jails.get(name) != null;
+	}
+	
+	/**
+	 * Returns whether or not the player is creating a jail or a cell.
+	 * 
+	 * <p>
+	 * 
+	 * If you want to check to see if they're just creating a jail then use {@link #isCreatingAJail(name) isCreatingAJail} or if you want to see if they're creating a cell then use {@link #isCreatingACell(name) isCreatingACell}. 
+	 * 
+	 * @param name The name of the player, in any case as we convert it to lowercase.
+	 * @return True if the player is creating a jail or cell, false if they're not creating anything.
+	 */
+	public boolean isCreatingSomething(String name) {
+		return this.jailCreators.containsKey(name.toLowerCase()) || this.cellCreators.containsKey(name.toLowerCase());
+	}
+	
+	/** Returns whether or not someone is creating a <strong>Jail</strong>. */
+	public boolean isCreatingAJail(String name) {
+		return this.jailCreators.containsKey(name.toLowerCase());
+	}
+	
+	/**
+	 * Method for setting a player to be creating a Jail, returns whether or not they were added successfully.
+	 * 
+	 * @param name The player who is creating a jail.
+	 * @return True if they were added successfully, false if they are already creating a Jail.
+	 */
+	public boolean addCreatingJail(String player, String jailName) {
+		if(isCreatingAJail(player)) {
+			return false;
+		}else {
+			this.jailCreators.put(player.toLowerCase(), new CreationPlayer(jailName));
+			return true;
+		}
+	}
+	
+	/** Returns the instance of the CreationPlayer for this player, null if there was none found. */
+	public CreationPlayer getJailCreationPlayer(String name) {
+		return this.jailCreators.get(name.toLowerCase());
+	}
+	
+	/** Returns whether or not someone is creating a <strong>Cell</strong>. */
+	public boolean isCreatingACell(String name) {
+		return this.cellCreators.containsKey(name.toLowerCase());
+	}
+	
+	/**
+	 * Method for setting a player to be creating a Jail, returns whether or not they were added successfully.
+	 * 
+	 * @param name The player who is creating a jail.
+	 * @return True if they were added successfully, false if they are already creating a Jail.
+	 */
+	public boolean addCreatingCell(String player, String cellName) {
+		if(isCreatingACell(player)) {
+			return false;
+		}else {
+			this.cellCreators.put(player.toLowerCase(), new CreationPlayer(cellName));
+			return true;
+		}
+	}
+	
+	/** Returns the instance of the CreationPlayer for this player, null if there was none found. */
+	public CreationPlayer getCellCreationPlayer(String name) {
+		return this.cellCreators.get(name.toLowerCase());
 	}
 }
--- src/com/graywolf336/jail/command/commands/JailCreateCommand.java
+++ src/com/graywolf336/jail/command/commands/JailCreateCommand.java
@@ -1,8 +1,10 @@
 package com.graywolf336.jail.command.commands;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.graywolf336.jail.JailManager;
+import com.graywolf336.jail.beans.CreationPlayer;
 import com.graywolf336.jail.command.Command;
 import com.graywolf336.jail.command.CommandInfo;
 
@@ -17,16 +19,39 @@ import com.graywolf336.jail.command.CommandInfo;
 public class JailCreateCommand implements Command {
 
 	public boolean execute(JailManager jm, CommandSender sender, String... args) {
+		Player player = (Player) sender;
 		String name = args[0];
 		
 		if(jm.isValidJail(name)) {
-			sender.sendMessage("ail with that name already exist!");
+			sender.sendMessage("Jail with that name already exist!");
 			return true;
 		}else {
 			//Create jail and all that good stuff here
+			if(jm.addCreatingJail(player.getName(), name)) {
+				sender.sendMessage("");
+			}else {
+				CreationPlayer cp = jm.getJailCreationPlayer(player.getName());
+				String message = "You're already creating a Jail with the name '" + cp.getName() + "' and you still need to ";
+				
+				switch(cp.getState()) {
+					case 1:
+						message += "select the first point.";
+						break;
+					case 2:
+						message += "select the second point.";
+						break;
+					case 3:
+						message += "set the teleport in location.";
+						break;
+					case 4:
+						message += "set the release location.";
+						break;
+				}
+				
+				player.sendMessage(message);
+			}
+			
+			return true;
 		}
-		
-		return true; //If they made it this far, the command is intact and ready to be processed. :)
 	}
-
 }
