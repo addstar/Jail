3462a73e5e0b17db24436b8cecc6be1fe8e011c7
graywolf336 <graywolf336@craftyn.com>
2013-09-17 10:25:27 -0500

    Add a first unit test on the Util.isInsideAB.
    Just testing out some unit testing and see if they work in the current
    Jenkins setup.

---
+++ src/test/java/com/graywolf336/jail/TestVectorsInside.java
@@ -0,0 +1,39 @@
+package test.java.com.graywolf336.jail;
+
+import static org.junit.Assert.*;
+
+import org.bukkit.util.Vector;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.graywolf336.jail.Util;
+
+public class TestVectorsInside {
+	private Vector bottomCorner;
+	private Vector topCorner;
+	
+	@Before
+	public void setUp() {
+		bottomCorner = new Vector(-10.50, 50.25, 100.00);
+		topCorner = new Vector(50, 100, 250);
+	}
+	
+	@Test
+	public void testIsInsideAB() {
+		Vector inside = new Vector(35, 64, 110);
+		assertTrue(Util.isInsideAB(inside, bottomCorner, topCorner));
+	}
+	
+	@Test
+	public void testIsOutsideAB() {
+		Vector outside = new Vector(34, 15, 350);
+		assertFalse(Util.isInsideAB(outside, bottomCorner, topCorner));
+	}
+	
+	@After
+	public void tearDown() {
+		bottomCorner = null;
+		topCorner = null;
+	}
+}
