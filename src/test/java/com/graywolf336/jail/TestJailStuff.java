fa9b9346e8d1a1e4ca443bd389909de93f978dcf
graywolf336 <graywolf336@craftyn.com>
2013-09-20 10:41:37 -0500

    Add another test. :)

--- src/test/java/com/graywolf336/jail/TestJailStuff.java
+++ src/test/java/com/graywolf336/jail/TestJailStuff.java
@@ -1,7 +1,13 @@
 package test.java.com.graywolf336.jail;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.junit.After;
 import org.junit.Before;
@@ -36,5 +42,26 @@ public class TestJailStuff {
 	public void testForJails() {
 		assertNotNull("The JailManager is null.", main.getJailManager());
 		assertNotNull("The HashSet for jails return is null.", main.getJailManager().getJails());
+	}
+	
+	@Test
+	public void testAddingJail() {
+		Player player = mock(Player.class);
+		when(player.getName()).thenReturn("graywolf336");
+		when(player.hasPermission(anyString())).thenReturn(true);
+		when(player.isOp()).thenReturn(true);
+		
+		CommandSender sender = (CommandSender) player;
+		when(sender.hasPermission(anyString())).thenReturn(true);
+		when(sender.isOp()).thenReturn(true);
+		
+		Command command = mock(Command.class);
+		when(command.getName()).thenReturn("jailcreate");
+		String[] args = { "testJail" };
+		
+		assertTrue(main.onCommand(sender, command, "jailcreate", args));
+		verify(player).sendMessage(ChatColor.AQUA + "----------Jail Zone Creation----------");
+		verify(player).sendMessage(ChatColor.GREEN + "First, you must select jail cuboid. Select the first point of the cuboid by right clicking on the block with your wooden sword. DO NOT FORGET TO MARK THE FLOOR AND CEILING TOO!");
+		verify(player).sendMessage(ChatColor.AQUA + "--------------------------------------");
 	}
 }
